baaed8a8d723d270ab86a23c56e16d1ebb72dfff
kobmat <kobmat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-28 14:42:11 +0000

    Tagging version 1.0.8
    git-svn-id: https://plugins.svn.wordpress.org/mathtex-equation-editor/tags/1.0.8@866979 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html/img/fractions/symbols.php
@@ -0,0 +1,13 @@
+<?php
+$fractions = Array(
+	"abc.gif" => array("_{}^{}\\textrm{#}"),
+	"frac.gif" => array("\\frac{#}{}"),
+	"tfrac.gif" => array("\\tfrac{#}{}"),
+	"xa.gif" => array(" #^{}"),
+	"xab.gif" => array(" #_{}^{}"),
+	"xa_b.gif" => array("{#_{}}^{}"),
+	"xsub.gif" => array("_{#}"),
+	"sqrt.gif" => array("\\sqrt{#}"),
+	"nroot.gif" => array("\\sqrt[]{#}"),
+);
+?>
