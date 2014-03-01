9a5bf16e36cc0f4ea58b76c3dabb9f64535bee9a
kobmat <kobmat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-01 01:36:19 +0000

    Tagging Version 1.0.9
    git-svn-id: https://plugins.svn.wordpress.org/mathtex-equation-editor/tags/1.0.9@867324 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html/img/arrows/symbols.php
@@ -0,0 +1,24 @@
+<?php
+$arrows = Array(
+	"leftarrow.gif" => array("\\leftarrow"),
+	"leftharpoondown.gif" => array("\\leftharpoondown"),
+	"leftharpoonup.gif" => array("\\leftharpoonup"),
+	"leftrightarrow.gif" => array("\\leftrightarrow"),
+	"leftrightharpoons.gif" => array("\\leftrightharpoons"),
+	"mapsto.gif" => array("\\mapsto"),
+	"oversetleftarrow.gif" => array("\\overset{a#}{\\leftarrow}"),
+	"oversetright.gif" => array("\\overset{a#}{\\rightarrow}"),
+	"rightarrow.gif" => array("\\rightarrow"),
+	"rightharpoondown.gif" => array("\\rightharpoondown"),
+	"rightharpoonup.gif" => array("\\rightharpoonup"),
+	"rightleftharpoons.gif" => array("\\rightleftharpoons"),
+	"to.gif" => array("\\to"),
+	"uLeftarrow.gif" => array("\\Leftarrow"),
+	"uLeftrightarrow.gif" => array("\\Leftrightarrow"),
+	"undersetleft.gif" => array("\\undersetleft"),
+	"undersetright.gif" => array("\\undersetright"),
+	"uRightarrow.gif" => array("\\Rightarrow"),
+	"xleftarrow.gif" => array("\\xleftarrow[a#]{b}"),
+	"xrightarrow.gif" => array("\\xrightarrow[a#]{b}"),
+)
+?>
---
+++ html/img/brackets/symbols.php
@@ -0,0 +1,12 @@
+<?php
+$brackets = Array(
+	"angle.gif" => array("\\left ( # \\right )"),
+	"bar.gif" => array("\\left \\| # \\right \\|"),
+	"bracket.gif" => array("\\left [ # \\right ]"),
+	"curlybrackets.gif" => array("\\left \\{ # \\right \\}"),
+	"lceil.gif" => array("\\left \\lceil # \\right \\rceil"),
+	"lfloor.gif" => array("\\left \\lfloor # \\right \\rfloor"),
+	"parentheses.gif" => array("\\left ( # \\right )"),
+	"twobar.gif" => array("\\left \\| # \\right \\|"),
+);
+?>
